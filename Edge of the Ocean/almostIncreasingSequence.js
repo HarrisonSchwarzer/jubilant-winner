4b52e61211a9ae23e89427f4bc08a96d4ea7bd16
realLizardPerson <harrisonschwarzer@gmail.com>
2019-06-17 07:26:07 -0500

    added linting for style

--- Edge of the Ocean/almostIncreasingSequence.js
+++ Edge of the Ocean/almostIncreasingSequence.js
@@ -1,26 +1,11 @@
-function almostIncreasingSequence(sequence) {
-  console.log('Oh boy! Look at this array:', sequence);
-  console.log(`It's ${sequence.length} elements long!`);
-  if (sequence.length == 2) {
-    return true
-  }
-  let baddies = 0;
-  for (i=0; i<sequence.length; i++) {
-    if (baddies > 1) {
-      return false
-    }
-    if (sequence[i] <= sequence[i-1]) {
-      console.log(`I caught one at index ${i}! Take a look:`, sequence[i], sequence[i-1]);
-      baddies += 1;
-      if (sequence[i+1] <= sequence[i-1]) {
-        return false
-      }
-    }
-  }
-  return true
+function almostIncreasingSequence(seq) {
+  let bad = 0;
+  for (let i = 1; i < seq.length; i+= 1) {if (seq[i] <= seq[i - 1]) {
+      bad++;
+      if (bad > 1) return false;
+      if (seq[i] <= seq[i - 2] && seq[i + 1] <= seq[i - 1]) return false;
+    }}
+  return true;
 }
 
-console.log(
-  almostIncreasingSequence([1, 3, 5, 3, 5])
-);
-
+console.log(almostIncreasingSequence([1, 2, 5, 3, 5]));
