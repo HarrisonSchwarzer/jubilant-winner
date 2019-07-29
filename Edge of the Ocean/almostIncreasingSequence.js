60b2199180cea0b73db8f0b58009e92683168a52
realLizardPerson <harrisonschwarzer@gmail.com>
2019-07-29 06:10:59 -0500

    just stole a solution out for almostIncreasingSequence so I could keep working

--- Edge of the Ocean/almostIncreasingSequence.js
+++ Edge of the Ocean/almostIncreasingSequence.js
@@ -1,11 +1,9 @@
 function almostIncreasingSequence(seq) {
-  let bad = 0;
-  for (let i = 1; i < seq.length; i += 1) {if (seq[i] <= seq[i - 1]) {
-      bad++;
-      if (bad > 1) return false;
-      if (seq[i] <= seq[i - 2] && seq[i + 1] <= seq[i - 1]) return false;
-    }}
-  return true;
+  var bad=0
+  for(var i=1;i<seq.length;i++) if(seq[i]<=seq[i-1]) {
+    bad++
+    if(bad>1) return false
+    if(seq[i]<=seq[i-2]&&seq[i+1]<=seq[i-1]) return false
+  }
+  return true
 }
-
-console.log(almostIncreasingSequence([1, 2, 5, 3, 5]));
