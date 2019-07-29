fe27756175c292224ddf0c40feb331ffd7786022
realLizardPerson <harrisonschwarzer@gmail.com>
2019-07-29 06:06:22 -0500

    another incorrect change

--- Edge of the Ocean/almostIncreasingSequence.js
+++ Edge of the Ocean/almostIncreasingSequence.js
@@ -1,6 +1,6 @@
 function almostIncreasingSequence(seq) {
   let bad = 0;
-  for (let i = 1; i < seq.length; i+= 1) {if (seq[i] <= seq[i - 1]) {
+  for (let i = 1; i < seq.length; i += 1) {if (seq[i] <= seq[i - 1]) {
       bad++;
       if (bad > 1) return false;
       if (seq[i] <= seq[i - 2] && seq[i + 1] <= seq[i - 1]) return false;
